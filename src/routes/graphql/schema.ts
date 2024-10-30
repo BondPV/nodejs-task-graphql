ff5a817aeb69e4814873cb68c3b349ba73bcb145
BondPV <bondpv@yandex.ru>
2024-10-30 21:56:37 +0300

    init: start graphql task

---
+++ src/routes/graphql/schema.ts
@@ -0,0 +1,20 @@
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { userType } from './types/user.js';
+import { UUIDType } from './types/uuid.js';
+
+const query = new GraphQLObjectType({
+name: 'Query',
+fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+    },
+    users: {
+      type: new GraphQLList(userType),
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({ query });
